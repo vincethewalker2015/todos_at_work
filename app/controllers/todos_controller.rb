ea5f7aea4bf58bacef5dce5e79cda43ebe0fac73
vinny <vwalker@hihostels.com>
2019-10-04 16:44:51 +0100

    First commit

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,26 @@
+class TodosController < ApplicationController
+
+	def index
+		@todos = Todo.all
+	end
+
+	def new
+		@todo = Todo.new
+	end
+
+	def create
+		@todo = Todo.new(todo_params)
+		if @todo.save
+			redirect_to todo_path(@todo)
+		else
+			render 'new'
+		end
+	end
+
+	private
+
+	def todo_params
+		params.require(:todo).permit(:name, :description)
+	end
+
+end
