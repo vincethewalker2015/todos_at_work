21c94e8fe3ff1502a25c13239db525e17a927328
vinny <vwalker@hihostels.com>
2019-10-17 14:44:00 +0100

    Created User migration, model and controllers and view files

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-  get 'todos/index'
-  get 'todos/new'
-  get 'todos/create'
-  get 'todos/show'
-  get 'todos/edit'
-  get 'todos/update'
-  get 'todos/destroy'
+
+  get 'users/new'
+  get 'users/create'
   root 'pages#home'
+  resources :users
   resources :todos
 end
