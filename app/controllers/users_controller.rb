3db6d6ca5b875c34a0c32a23c3c30a9d1ec6dd61
vincentbwalker <vincethewalker@gmail.com>
2019-10-21 21:06:51 +0100

    Added User Show and Edit Pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,24 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash.now[:success] = "Your Details have been updated"
+      redirect_to user_path(@user)
+    else
+      render 'edit'
+    end
+  end
+
     private
 
     def user_params
