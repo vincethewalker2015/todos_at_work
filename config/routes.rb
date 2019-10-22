b03149d3d300c3209d435678144087e3d98646c3
vincentbwalker <vincethewalker@gmail.com>
2019-10-22 20:00:28 +0100

    Added Session Controller for Login

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   resources :users
   resources :todos
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/login', to: 'sessions#destroy'
 end
